638cb4f0eb8bd19b55ef911e8d2db06e6bca7f44
sergei888 <sergeiclashroale@gmail.com>
2022-09-12 23:36:31 +0200

    Complete lab1

---
+++ lab_1/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by sergei888 on 9/12/22.
+//
+
+#include <iostream>
+
+int main() {
+    short first_side;
+
+    std::cout << "Введите первую сторону прямоугольника:\n";
+    std::cin >> first_side;
+
+    short second_side;
+
+    std::cout << "Введите вторую сторону прямоугольника:\n";
+    std::cin >> second_side;
+
+    std::cout << "Периметр прямоугольника: " << first_side * 2 + second_side * 2;
+
+    return 0;
+}

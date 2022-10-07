d32d2171911d84b016c4e6f8f9a7db872bb270b1
Sergei888 <sergeiclashroale@gmail.com>
2022-10-07 12:55:05 +0200

    Unused include

--- lab_4/lab_4.2.cpp
+++ lab_4/lab_4.2.cpp
@@ -2,6 +2,7 @@
 // Created by Sergei Kuzmenkov on 23.09.2022.
 //
 
+#include <iostream>
 
 /*
  * Ввести последовательность натуральных чисел {Aj}j=1...n (n<=1000).
@@ -10,6 +11,62 @@
  * числа с одинаковыми первыми цифрами и одинаковыми наибольшими цифрами дополнительно упорядочить по неубыванию самого числа.
  * */
 
+int getMaxNumeral(int i) {
+    int max = 0;
+    while (i > 0) {
+        int numeral = i % 10;
+        if (numeral > max) {
+            max = numeral;
+        }
+        i /= 10;
+    }
+    return max;
+}
+
+int getFirstNumeral(int i) {
+    while (i > 9) {
+        i /= 10;
+    }
+    return i;
+}
+
+bool compare(int i, int j) {
+    if (getFirstNumeral(i) != getFirstNumeral(j)) {
+        return getFirstNumeral(i) < getFirstNumeral(j);
+    }
+
+    if (getMaxNumeral(i) != getMaxNumeral(j)) {
+        return getMaxNumeral(i) < getMaxNumeral(j);
+    }
+
+    return i < j;
+}
+
+
+void printArray(int series[], int length) {
+    std::cout << "[";
+    for (int i = 0; i < length; i++) {
+        std::cout << series[i];
+        if (i + 1 < length) {
+            std::cout << " ";
+        }
+    }
+    std::cout << "]\n";
+}
+
 int main() {
+    std::cout << "Введите длинну последовательности: ";
+    int n;
+    std::cin >> n;
+
+    int series[n];
+    for (int i = 0; i < n; i++) {
+        std::cout << "Введите число последовательности: ";
+        std::cin >> series[i];
+    }
+
+    std::sort(series, series + n, compare);
+    printArray(series, n);
+
     return 0;
 }
--- lab_4/lab_4.3.cpp
+++ lab_4/lab_4.3.cpp
@@ -2,11 +2,62 @@
 // Created by Sergei Kuzmenkov on 30.09.2022.
 //
 
+#include "string.h"
+#include <iostream>
+
 /*
  * Дана целочисленная матрица {Aij}i=1..n,j=1..m (n,m<=100).
  * Найти столбец содержащий наименьший элемент матрицы и заменить все отрицательные элементы этого столбца числом 0.
  * */
 
 int main() {
+    std::cout << "Введите количество строк матрицы: ";
+    int rows;
+    std::cin >> rows;
+
+    std::cout << "Введите количество столбцов матрицы: ";
+    int columns;
+    std::cin >> columns;
+
+    int matrix[rows][columns];
+    int minElement = INT32_MAX;
+    int minElementColumn = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            int element;
+            std::cout << "Введите элемент матрицы [" << i << "][" << j << "]: ";
+            std::cin >> element;
+            matrix[i][j] = element;
+
+            if (element < minElement) {
+                minElement = element;
+                minElementColumn = j;
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Миниммальный элемент матрицы: " << minElement << std::endl;
+    std::cout << "Столбец с минимальным элементом матрицы: " << minElementColumn + 1 << std::endl;
+
+    for (int i = 0; i < rows; ++i) {
+        if (matrix[i][minElementColumn] < 0) {
+            matrix[i][minElementColumn] = 0;
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }

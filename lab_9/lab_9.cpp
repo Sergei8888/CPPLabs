02b29dde91ea46b148a20abd6484cea16a1cb679
Sergei888 <sergeiclashroale@gmail.com>
2022-12-10 12:05:07 +0200

    Finished lab 9

--- lab_9/lab_9.cpp
+++ lab_9/lab_9.cpp
@@ -1,128 +1,78 @@
-//
-// Created by Sergei Kuzmenkov on 23.11.2022.
-//
-
-/*
- * Ввести последовательность натуральных чисел {Aj}j=1...n (n<=1000).
- * Упорядочить последовательность по неубыванию первой цифры числа,
- * числа с одинаковыми первыми цифрами дополнительно упорядочить по неубыванию наибольшей цифры числа,
- * числа с одинаковыми первыми цифрами и одинаковыми наибольшими цифрами дополнительно упорядочить по неубыванию самого числа.
- * */
-
+#include <cmath>
 #include <iostream>
+#include <limits>
 
-void printArray(int series[], int length) {
-    std::cout << "[";
-    for (int i = 0; i < length; i++) {
-        std::cout << series[i];
-        if (i + 1 < length) {
-            std::cout << " ";
+class Triangle {
+private:
+    double a;
+    double b;
+    double c;
+public:
+
+    Triangle(double a, double b, double c) {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+
+    double getArea() const {
+        double p = (a + b + c) / 2;
+        double S = sqrt(p * (p - a) * (p - b) * (p - c));
+        return S;
+    };
+
+    void print() const {
+        std::cout << "Triangle: " << a << ", " << b << ", " << c << std::endl;
+        std::cout << "Area: " << getArea() << std::endl;
+    };
+};
+
+class TriangleFactory {
+public:
+    static Triangle *getTriangleFromConsole() {
+        double a, b, c;
+
+        while (true) {
+            a = getSide(1);
+            b = getSide(2);
+            c = getSide(3);
+
+            bool triangleExists = a + b > c && a + c > b && b + c > a;
+            if (!triangleExists) {
+                std::cout << "Треугольник не существует" << std::endl;
+                continue;
+            }
+
+            break;
         }
-    }
-    std::cout << "]\n";
-}
 
-int getFirstNumeral(int number) {
-    while (number > 9) {
-        number /= 10;
+        return new Triangle(a, b, c);
     }
-    return number;
-}
 
-int getMaxNumeral(int number) {
-    int maxNumeral = 0;
-    while (number > 0) {
-        int numeral = number % 10;
-        if (numeral > maxNumeral) {
-            maxNumeral = numeral;
+private:
+    static double getSide(int sideNumber) {
+        double a;
+        while (true) {
+            std::cout << "Введите " << sideNumber << " сторону треугольника: ";
+            std::cin >> a;
+            if (std::cin.fail()) {
+                std::cout << "Некорректный ввод " << sideNumber << " стороны" << std::endl;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+
+            break;
         }
-        number /= 10;
+
+        return a;
     }
-    return maxNumeral;
-}
+};
 
 int main() {
-    std::cout << "Введите длинну последовательности: ";
-    int n = *new int;
-    std::cin >> n;
-
-    // Arrays
-    int *series = new int[n];
-    int *firstNumerals = new int[n];
-    int *maxNumerals = new int[n];
-    
-
-    for (int i = 0; i < n; i++) {
-        // Getting number from the console
-        std::cout << "Введите число последовательности: ";
-        std::cin >> series[i];
-
-        firstNumerals[i] = getFirstNumeral(series[i]);
-        maxNumerals[i] = getMaxNumeral(series[i]);
-    }
-
-    std::cout << "Исходная последовательность: ";
-    printArray(series, n);
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            // Sorting by first numeral
-            if (firstNumerals[i] != firstNumerals[j]) {
-                if (firstNumerals[i] > firstNumerals[j]) {
-                    std::swap(series[i], series[j]);
-                    std::swap(firstNumerals[i], firstNumerals[j]);
-                    std::swap(maxNumerals[i], maxNumerals[j]);
-                }
-
-                continue;
-            }
-
-            // Sorting by max numeral
-            if (maxNumerals[i] != maxNumerals[j]) {
-                if (maxNumerals[i] > maxNumerals[j]) {
-                    std::swap(series[i], series[j]);
-                    std::swap(firstNumerals[i], firstNumerals[j]);
-                    std::swap(maxNumerals[i], maxNumerals[j]);
-                }
-
-                continue;
-            }
-
-            // Sorting by number
-            if (series[i] > series[j]) {
-                std::swap(series[i], series[j]);
-                std::swap(firstNumerals[i], firstNumerals[j]);
-                std::swap(maxNumerals[i], maxNumerals[j]);
-            }
-        }
-    }
-
-    std::cout << "Отсортированная последовательность: ";
-    printArray(series, n);
-
-    std::cout << "Введите новую длинну последовательности: ";
-    int newN = *new int;
-    std::cin >> newN;
-
-    if (newN <= n) {
-        std::cout << "Новая последовательность: .\n";
-        printArray(series, newN);
-    } else {
-        int *newSeries = new int[newN];
-
-        // Copying old series
-        for (int i = 0; i < n; i++) {
-            newSeries[i] = series[i];
-        }
-        // Adding new numbers
-        for (int i = n; i < newN; i++) {
-            std::cout << "Введите недостающее значение последовательности: ";
-            std::cin >> newSeries[i];
-        }
-
-        std::cout << "Новая последовательность: \n";
-        printArray(newSeries, newN);
-    }
+    Triangle *triangle = TriangleFactory::getTriangleFromConsole();
+    triangle->print();
+    delete triangle;
 
     return 0;
 }
